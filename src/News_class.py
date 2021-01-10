555cc61fc62b970b292838965d79e55bef519191
Ibrahim V <35322625+IbraTebas@users.noreply.github.com>
2021-01-11 00:31:19 +1000

    class News separated and amended

---
+++ src/News_class.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 00:24:41 2021
+
+@author: ibrah
+"""
+class News:
+
+    def __init__(self):
+        self.day = ''
+        self.time = ''
+        self.title = ''
+        self.subtitle = ''
+        self.category = ''
+        self.description = ''
+        self.author = ''
+        self.link = ''
