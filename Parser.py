676f51c993b1e4f7d5eae914ef0e2e9746c7c997
Ibrahim V <35322625+IbraTebas@users.noreply.github.com>
2021-01-10 17:47:23 +1000

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -1,5 +1,3 @@
-import pandas as pd
-pd.set_option('max_colwidth', None)
 import xml.etree.ElementTree as ET
 
 dict_month = {'Jan': "01", 'Feb':'02', 'Mar':'03','Apr': '04','May':'05', 'Jun':'06', 'Jul':'07', 'Aug':'08', 'Sep':'09', 'Oct':'10', 'Nov':'11', 'Dic':'12'}
@@ -30,38 +28,32 @@ class XMLParser:
         self.doc = ET.parse(path)
         self.root = self.doc.getroot()
 
+    def remove_unicode(self, text):
+        return text.replace("&nbsp;", "").replace('&gt', "").replace("<br>", "")
+
+
+    def parser_text(self, tag, element):
+        search = element.find(tag)
+        if search is not None:
+            return self.remove_unicode(search.text)
+        return ''
+
+
     def parse(self):
         parsing = []
         items = self.root.find('channel').findall('item')
         for child in items:
             new = News()
             # Title section parser
-            new.title = child.find('title').text
-            new.category = child.find('category').text
-
-            # Description section parser
-            if child.find('description') is None:
-                pass
-            else:
-                new.description = child.find('description').text.replace("&nbsp;", "")
-                new.description = new.description.replace("&gt", "")
-                new.description = new.description.replace("<br>", "")
-                # Day and time section, converting at the same format of the file name
-            new.day = child.find('pubDate').text[5:16]
+            new.title = self.parser_text('title', child)
+            new.category = self.parser_text('category', child)
+            new.description = self.parser_text('description', child)
+            new.day = self.parser_text('pubDate', child)[5:16]
+            new.time = self.parser_text('pubDate', child)[17:22]
             new.day = format_date_elpais(new.day)
-            new.time = child.find('pubDate').text[17:22]
-            new.link = child.find('link').text
-            # Author section
-            if child.find('author') is None:
-                pass
-            else:
-                new.author = child.find('author').text
-                # Subtitle section
-            if child.find('subtitle') is None:
-                pass
-            else:
-                new.subtitle = child.find('subtitle').text
-                new.subtitle = new.subtitle.capitalize()
+            new.link = self.parser_text('link', child)
+            new.author = self.parser_text('author', child)
+            new.subtitle = self.parser_text('subtitle', child).capitalize()
             parsing.append(new)
         return parsing
 
@@ -81,11 +73,16 @@ class XMLParser:
 #   lst=['title', 'description', 'pubDate', 'link', 'subtitle', 'category','categorySlug', 'parentCategorySlug', 'articleSlug', 'author', 'imageCaption']
 
 News=XMLParser('Elpais-03-01-2021-22hs45min.xml').parse()
+#
+# df = pd.DataFrame([t.__dict__ for t in News ])        ###linea fundamental, esto convierte los objetos de la clase (es decir las noticias) y las vierte en una pandas dataframe.
+#
+# df2=pd.DataFrame([vars(s) for s in News])              ###linea fundamental, esto convierte los objetos de la clase (es decir las noticias) y las vierte en una pandas dataframe variacion mas conservadora parece en terminso quer toma todo
 
-df = pd.DataFrame([t.__dict__ for t in News ])        ###linea fundamental, esto convierte los objetos de la clase (es decir las noticias) y las vierte en una pandas dataframe.
+# df                                          ###se podria hacer lo siguiente para quedarse con las columnas que quieras: *****pd.DataFrame([vars(s) for s in signals], columns=['x', 'y'])***
+#
+# df
+import pandas as pd
+pd.set_option('max_colwidth', None)
+df2=pd.DataFrame([vars(s) for s in News])
+df2
 
-df2=pd.DataFrame([vars(s) for s in News])              ###linea fundamental, esto convierte los objetos de la clase (es decir las noticias) y las vierte en una pandas dataframe variacion mas conservadora parece en terminso quer toma todo
-
-df                                          ###se podria hacer lo siguiente para quedarse con las columnas que quieras: *****pd.DataFrame([vars(s) for s in signals], columns=['x', 'y'])***
-
-df
