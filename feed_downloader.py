d8af67fdfc0a1d8e02f030d2a36b09ded4c57b91
Martin Rodriguez <martin.rdfg@gmail.com>
2021-01-11 19:10:27 -0300

    feeds files

--- feed_downloader.py
+++ feed_downloader.py
@@ -8,6 +8,10 @@ def main():
     feed_brecha = 'https://brecha.com.uy/feed/'
     feed_el_observador = "https://www.elobservador.com.uy/rss/elobservador.xml"
     feed_la_diaria = 'https://ladiaria.com.uy/feeds/articulos/'
+    feed_mvd_com_destacadas = 'https://www.montevideo.com.uy/anxml.aspx?58'
+    feed_mvd_com_news = 'https://www.montevideo.com.uy/anxml.aspx?59'
+    feed_mvd_com_opinion = 'https://www.montevideo.com.uy/anxml.aspx?1303'
+    feed_busqueda = 'https://www.busqueda.com.uy/anxml.aspx?13'
     today = "{}-{}-{}".format(str(pd.to_datetime("today").date())[8:10], str(pd.to_datetime("today").date())[5:7],
                               str(pd.to_datetime("today").date())[0:4])
     now = str(pd.to_datetime("today").time())[0:5]
@@ -23,6 +27,19 @@ def main():
     response = requests.get(feed_la_diaria)
     with open('./feeds/ladiaria/ladiaria-{}-{}hs{}min.xml'.format(today, now[0:2], now[3:5]), 'wb') as file:
         file.write(response.content)
+    response = requests.get(feed_mvd_com_news)
+    with open('./feeds/mvdcom/mvd_com_news-{}-{}hs{}min.xml'.format(today, now[0:2], now[3:5]), 'wb') as file:
+        file.write(response.content)
+    response = requests.get(feed_mvd_com_destacadas)
+    with open('./feeds/mvdcom/mvd_com_destacadas-{}-{}hs{}min.xml'.format(today, now[0:2], now[3:5]), 'wb') as file:
+        file.write(response.content)
+    response = requests.get(feed_mvd_com_opinion)
+    with open('./feeds/mvdcom/mvd_com_opinion-{}-{}hs{}min.xml'.format(today, now[0:2], now[3:5]), 'wb') as file:
+        file.write(response.content)
+    response = requests.get(feed_busqueda)
+    with open('./feeds/busqueda/busqueda-{}-{}hs{}min.xml'.format(today, now[0:2], now[3:5]), 'wb') as file:
+        file.write(response.content)
+
     print('file downloaded successfully')
 
 
